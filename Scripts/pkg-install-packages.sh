adaa2d4ae55626134a0119dd55613262ff42ddce
coevoe <grastlinka@gmail.com>
2022-10-20 19:59:49 +0200

    Added script to automaticly install packages from pkglist.txt file

---
+++ Scripts/pkg-install-packages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -xe
+
+if [ "$(which paru)" = "/usr/bin/paru" ]; then
+  $(paru -S --needed - < pkglist.txt)
+else
+  $(git clone https://aur.archlinux.org/paru.git)
+  $(cd paru && makepkg -sci)
+  $(echo -e "DONE")
