e6beaf86ae133657305465cbac69e26fbf05702c
coevoe <grastlinka@gmail.com>
2022-10-21 16:25:24 +0200

    Corrected an error in the script

--- Scripts/pkg-install-packages.sh
+++ Scripts/pkg-install-packages.sh
@@ -4,18 +4,18 @@ set -xe
 # This function install paru for you if you do not have it 
 
 Paruinstlall() {
-  $(git clone https://aur.archlinux.org/paru.git)
-  $(cd paru && makepkg -sci)
-  $(echo -e "########")
-  $(echo -e "# DONE #")
-  $(echo -e "########\n")
+  git clone https://aur.archlinux.org/paru.git
+  cd paru && makepkg -sci
+  echo -e "########"
+  echo -e "# DONE #"
+  echo -e "########\n"
   Install
 }
 
 # This function install all my packages on to your system
 
 Install() {
-  $(paru -S --needed - < pkglist.txt)
+  paru -S --needed - < pkglist.txt
 }
 
 # This function run an if loop to check if you have installed 
