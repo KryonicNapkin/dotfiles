71c6ebf4f48bab69e29974ed01c95aef4f199b34
coevoe <grastlinka@gmail.com>
2022-10-21 15:21:40 +0200

    Added Docs and make the script better looking in the pkg-install-packages.sh

--- Scripts/pkg-install-packages.sh
+++ Scripts/pkg-install-packages.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 set -xe
 
-if [ "$(which paru)" = "/usr/bin/paru" ]; then
-  $(paru -S --needed - < pkglist.txt)
-else
+# This function install paru for you if you do not have it 
+
+Paruinstlall() {
   $(git clone https://aur.archlinux.org/paru.git)
   $(cd paru && makepkg -sci)
-  $(echo -e "DONE")
+  $(echo -e "########")
+  $(echo -e "# DONE #")
+  $(echo -e "########\n")
+  Install
+}
+
+# This function install all my packages on to your system
+
+Install() {
+  $(paru -S --needed - < pkglist.txt)
+}
+
+# This function run an if loop to check if you have installed 
+# paru and if not it run "Paruinstlall" function to install paru
+# When installing paru is done it run "Install" function described 
+# earlier in this file 
+
+if [ "$(which paru)" = "/usr/bin/paru" ]; then
+  Install
+else
+  Paruinstlall
 fi
+
+# End of script
