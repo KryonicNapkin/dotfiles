8b054cf044a1af7c1b6d23da2c95e4d5785d4b87
coevoe <grastlinka@gmail.com>
2022-10-23 17:32:34 +0200

    Corrected an error in pkg-fetch.sh

--- Scripts/pkg-fetch.sh
+++ Scripts/pkg-fetch.sh
@@ -6,7 +6,7 @@ if [ "$(ping -w 1 8.8.8.8 | awk '{print $1}' | head -n 2 | tail -n 1)" = 64 ]; t
   cd ..
   git add -u .
   git commit -m "Added the latest fetch of packages"
-  git push git@github.com:coevoe/My-dotfile.git 
+  git push git@github.com:coevoe/My-dotfiles.git 
 else
   echo "Error connecting to internet"
 fi
