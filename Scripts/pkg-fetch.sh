5c6dfd2fd18a407d512d1512463f44d68ffa01a2
coevoe <grastlinka@gmail.com>
2022-10-23 18:31:43 +0200

    Added the latest fetch of packages

--- Scripts/pkg-fetch.sh
+++ Scripts/pkg-fetch.sh
@@ -2,11 +2,17 @@
 set -xe
 
 if [ "$(ping -w 1 8.8.8.8 | awk '{print $1}' | head -n 2 | tail -n 1)" = 64 ]; then
-  paru -Qe | awk '{print $1}' > ../Bin/pkglist.txt
-  cd ..
-  git add -u .
-  git commit -m "Added the latest fetch of packages"
-  git push git@github.com:coevoe/My-dotfiles.git 
+  if [ $(diff ~/My-dotfiles/Bin/pkglist.txt ~/$(paru -Qe | awk '{print $1}' > $HOME/pkglist.txt)) = \n ]; then
+    paru -Qe | awk '{print $1}' > ~/My-dotfiles/Bin/pkglist.txt
+    cd ~/My-dotfiles/
+    git add -u .
+    git commit -m "Added the latest fetch of packages"
+    git push git@github.com:coevoe/My-dotfiles.git 
+  else
+    echo "nothing changed"
+  fi
 else
   echo "Error connecting to internet"
 fi
+
+rm ~/pkglist.txt
