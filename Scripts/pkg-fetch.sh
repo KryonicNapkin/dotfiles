a34d7afe528892fd269a747cdcb1d97caa2642c3
coevoe <grastlinka@gmail.com>
2022-10-23 17:05:54 +0200

    Added the latest fetch of packages

--- Scripts/pkg-fetch.sh
+++ Scripts/pkg-fetch.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+set -xe
 
+if [ "$(ping -w 1 8.8.8.8 | awk '{print $1}' | head -n 2 | tail -n 1)" = 64 ]; then
+  paru -Qe | awk '{print $1}' > ../Bin/pkglist.txt
+  cd ..
+  git add -u .
+  git commit -m "Added the latest fetch of packages"
+  git push git@github.com:coevoe/My-dotfile.git 
+fi
