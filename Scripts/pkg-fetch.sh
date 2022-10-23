686a7a69b06699f31d335d1daeba22e9e1927fe0
coevoe <grastlinka@gmail.com>
2022-10-23 18:41:46 +0200

    Added the latest fetch of packages

--- Scripts/pkg-fetch.sh
+++ Scripts/pkg-fetch.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -xe
 
+check() {
+  paru -Qe | awk '{print $1}' > ~/My-dotfiles/Bin/pkglist.txt
+  cd ~/My-dotfiles/    
+  git add -u .
+  git commit -m "Added the latest fetch of packages"
+  git push git@github.com:coevoe/My-dotfiles.git 
+}
+
 if [ "$(ping -w 1 8.8.8.8 | awk '{print $1}' | head -n 2 | tail -n 1)" = 64 ]; then
   if [ $(diff ~/My-dotfiles/Bin/pkglist.txt ~/$(paru -Qe | awk '{print $1}' > $HOME/pkglist.txt)) = \n ]; then
-    paru -Qe | awk '{print $1}' > ~/My-dotfiles/Bin/pkglist.txt
-    cd ~/My-dotfiles/
-    git add -u .
-    git commit -m "Added the latest fetch of packages"
-    git push git@github.com:coevoe/My-dotfiles.git 
-  else
+    check
+      else
     echo "nothing changed"
   fi
 else
