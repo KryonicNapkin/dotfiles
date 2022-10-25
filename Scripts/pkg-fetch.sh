9581664ca922377b21fbcb79e2b4fee777fd71ce
coevoe <grastlinka@gmail.com>
2022-10-25 17:36:52 +0200

    Change stuff in the script

--- Scripts/pkg-fetch.sh
+++ Scripts/pkg-fetch.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -xe
 
 check() {
   paru -Qe | awk '{print $1}' > ~/My-dotfiles/Bin/pkglist.txt
   cd ~/My-dotfiles/    
-  git add -u .
+  git add -u Bin/pkglist.txt 
   git commit -m "Added the latest fetch of packages"
   git push git@github.com:coevoe/My-dotfiles.git 
 }
@@ -12,8 +11,7 @@ check() {
 if [ "$(ping -w 1 8.8.8.8 | awk '{print $1}' | head -n 2 | tail -n 1)" = 64 ]; then
   check
 else
-  echo "Error connecting to internet"
+  dunstify "Error connecting to internet" -u critical
 fi
 
-rm ~/stdou_test.txt
 rm ~/pkglist.txt
