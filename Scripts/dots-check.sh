de147daef101a62a4a19d10c45fbef1e12b25bf0
coevoe <grastlinka@gmail.com>
2022-10-28 08:55:33 +0200

    Added home dir files to check into dot-check.sh script

--- Scripts/dots-check.sh
+++ Scripts/dots-check.sh
@@ -8,7 +8,7 @@ dotscheck() {
   echo -e "### Qtile dot dir ###\n"
   diff -q ~/.config/qtile/ ~/My-dotfiles/config.d/qtile/
   echo -e "\n### Dunst dot dir ###"
-  diff ~/.config/dunst/ ~/My-dotfiles/config.d/dunst/
+  diff -q ~/.config/dunst/ ~/My-dotfiles/config.d/dunst/
   echo -e "\n### zsh dot dir ###"
   diff -q ~/.config/zsh/ ~/My-dotfiles/config.d/zsh/
   echo -e "\n### alacritty dot dir ###"
@@ -16,6 +16,15 @@ dotscheck() {
   echo -e "\n### rofi dot dir ###"
   diff -q ~/.config/rofi/ ~/My-dotfiles/config.d/rofi/
   echo -e "##############################\n"
+  echo -e "##############################"
+  echo -e "### HOME DIRECTORY FILES ###"
+  echo -e "##############################\n"
+  echo -e "### .zshrc file ###\n"
+  diff -q ~/.zshrc ~/My-dotfiles/.zshrc
+  echo -e "### .bashrc file ###\n"
+  diff -q ~/.bashrc ~/My-dotfiles/.bashrc
+  echo -e "### .fehbg file ###\n"
+  diff -q ~/.fehbg ~/My-dotfiles/.fehbg
 }
 
 dotscheck
