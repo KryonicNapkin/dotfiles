e69e540c0eba59c1abfb0425a28e8b4f937adacb
coevoe <grastlinka@gmail.com>
2022-10-28 08:04:52 +0200

    Changed autostart.sh

--- config.d/qtile/autostart.sh
+++ config.d/qtile/autostart.sh
@@ -5,6 +5,6 @@ picom --no-fading-openclose --backend glx --config ~/.config/qtile/picom.conf &
 xfce4-power-manager &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 dunst &
-discrod --start-minimized &
+discord --start-minimized &
 thunderbird &
 artha &
