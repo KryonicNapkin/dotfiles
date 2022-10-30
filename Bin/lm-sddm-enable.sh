9a82589d60db1e98cfbc75d644a6e27c680bf867
coevoe <grastlinka@gmail.com>
2022-10-30 14:49:48 +0100

    Added prompt and enabling theme too

--- Bin/lm-sddm-enable.sh
+++ Bin/lm-sddm-enable.sh
@@ -4,12 +4,45 @@
 # This script enables my sddm theme and set sddm 
 # as your default login manager
 
+abort() {
+  echo "###########"
+  echo "# ABORTED #"
+  echo -e "###########\n"
+  exit 1
+}
+
 enablesddm() {
   sudo systemctl disable $(grep '/usr/bin' /etc/systemd/system/display-manager.service | awk -F / '{print $NF}')
   sudo systemctl enable sddm
   echo "##############################################"
   echo "# Enabled sddm as your default login manager #"
   echo -e "##############################################\n"
+  while true; do
+    read -p "Do you want to install theme too?: [yn]" choice1
+    case "$choice1" in
+      y) enablesddmtheme; break;;
+      n) abort ;;
+      *) echo "Please choice y or n:"
+    esac
+  done
 }
 
-enablesddm
+enablesddmtheme() {
+  sudo cp -rf ~/My-dotfiles/usr.d/sddm /usr/share/ 
+  sudo cp -rf ~/My-dotfiles/usr.d/sddm.conf.d /usr/lib/sddm/ 
+  echo "###############################################"
+  echo "# Enabled sugar-dark theme as your sddm theme #"
+  echo -e "###############################################\n"
+}
+
+main() {
+  while true; do
+  read -p "Do you want to install my config of sddm as your default login manager? [yn]: " choice
+    case "$choice" in
+      y) enablesddm; 
+      break;; 
+      n) abort;;
+      *) echo "Please choice y or n:"
+    esac
+  done
+}
