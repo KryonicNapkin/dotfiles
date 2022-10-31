6b1e60029812fc9eb37ec40768ec200ce926dae9
coevoe <grastlinka@gmail.com>
2022-10-31 15:09:50 +0100

    Changed default editor to nvim because lunarvim would not start

--- config.d/qtile/scripts/rofi-configs.sh
+++ config.d/qtile/scripts/rofi-configs.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 CONFDIR=$HOME/.config
-ED="lvim"
+ED="nvim"
 
 declare -a options=(
 "alacrity - $CONFDIR/alacritty/alacritty.yml"
+"dunst - $CONFDIR/dunst/dunstrc"
 "qtile - $CONFDIR/qtile/config.py"
 "autostart - $CONFDIR/qtile/autostart.sh"
-"zshrc - $HOME/.zshrc"
 "zsh-prompt - $CONFDIR/zsh/zsh-prompt"
 "zsh-aliases - $CONFDIR/zsh/zsh-aliases"
 "zsh-functions - $CONFDIR/zsh/zsh-functions"
 "zsh-exports - $CONFDIR/zsh/zsh-exports"
-"README.md - $HOME/My-dotfiles/README.md"
-"dunst - $CONFDIR/dunst/dunstrc"
-"quit"
+"README - $HOME/My-dotfiles/README.md"
+"fehbg - $HOME/.fehbg"
+"xinitrc - $HOME/.xinitrc"
+"zshrc - $HOME/.zshrc"
+"QUIT"
 )
 
 choice=$( printf "%s\n" "${options[@]}" | rofi -dmenu -p Edit -theme ~/.config/rofi/launchers/type-4/style-1_configs.rasi) 
