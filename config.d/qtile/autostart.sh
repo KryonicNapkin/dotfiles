ac26788695d20fee2fe3124fcbf628943a93e557
coevoe <grastlinka@gmail.com>
2022-11-02 12:51:06 +0100

    Changed xfce4-power-manager to mate-power-manager

--- config.d/qtile/autostart.sh
+++ config.d/qtile/autostart.sh
@@ -2,7 +2,7 @@
 
 picom --no-fading-openclose --backend glx --config ~/.config/picom/picom.conf &
 /home/thinker/.fehbg &
-xfce4-power-manager &
+mate-power-manager &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 dunst &
 discord --start-minimized &
