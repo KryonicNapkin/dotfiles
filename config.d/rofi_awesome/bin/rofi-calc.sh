3886072ab67f2b5497641ca6404b21f52d58b368
coevoe <grastlinka@gmail.com>
2023-04-09 19:42:51 +0200

    Renamed config and usr dirs

--- config.d/rofi_awesome/bin/rofi-calc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rofi -show calc -modi calc -no-show-match -no-sort -theme ~/.config/rofi_awesome/configs/style-1_calc.rasi
--- config.d/rofi_awesome/bin/rofi-emoji.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rofi -modi emoji -show emoji -theme ~/.config/rofi_awesome/configs/style-1_emoji.rasi
