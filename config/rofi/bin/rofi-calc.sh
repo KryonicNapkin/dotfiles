407c81e233aca356ef2db16288f92bb62d245d77
coevoe <grastlinka@gmail.com>
2023-04-10 17:15:33 +0200

    Moved bin colors and configs to the wm specific dirs

--- config/rofi/bin/rofi-calc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rofi -show calc -modi calc -no-show-match -no-sort -theme ~/.config/rofi/configs/style-1_calc.rasi
--- config/rofi/bin/rofi-emoji.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rofi -modi emoji -show emoji -theme ~/.config/rofi/configs/style-1_emoji.rasi
