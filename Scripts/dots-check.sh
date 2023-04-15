a6dd9e0350cc5ce4659a10e9fe8ea62f848129a4
coevoe <grastlinka@gmail.com>
2023-04-15 16:35:27 +0200

    Changed the place of the \n character

--- Scripts/dots-check.sh
+++ Scripts/dots-check.sh
@@ -6,15 +6,15 @@ CONFDIR=/home/oizero/.config
 dotscheck() {
 	echo -e "##############################"
 	echo -e "### CONFIG DIRECTORY FILES ###"
-	echo -e "##############################\n"
+	echo -e "##############################"
 
 	echo -e "\n### AwesomeWM dir ###"
 	diff -rq $CONFDIR/awesome/ ~/My-dotfiles/config/awesome/
 
-	echo -e "### Qtile dir ###"
+	echo -e "\n### Qtile dir ###"
 	diff -rq $CONFDIR/qtile/ ~/My-dotfiles/config/qtile/
 
-	echo -e "### DWM dir ###"
+	echo -e "\n### DWM dir ###n"
 	diff -rq $CONFDIR/dwm/ ~/My-dotfiles/config/dwm/
 
 	echo -e "\n### Dunst dir ###"
@@ -26,49 +26,49 @@ dotscheck() {
 	echo -e "\n### alacritty dir ###"
 	diff -rq $CONFDIR/alacritty/ ~/My-dotfiles/config/alacritty/
 
-	echo -e "### feh dir ###"
+	echo -e "\n### feh dir ###"
 	diff -rq $CONFDIR/feh/ ~/My-dotfiles/config/feh/
 
-	echo -e "### nvim dir ###"
+	echo -e "\n### nvim dir ###"
 	diff -rq $CONFDIR/nvim/ ~/My-dotfiles/config/nvim/
 
 	echo -e "###################################################################\n"
 
 	echo -e "############################"
 	echo -e "### HOME DIRECTORY FILES ###"
-	echo -e "############################\n"
+	echo -e "############################"
 
-	echo -e "### .Xresources file ###\n"
+	echo -e "\n### .Xresources file ###"
 	diff -rq ~/.Xresources ~/My-dotfiles/.Xresources
 
-	echo -e "### .bashrc file ###\n"
+	echo -e "\n### .bashrc file ###n"
 	diff -rq ~/.bashrc ~/My-dotfiles/.bashrc
 
-	echo -e "### .bash_profile file ###\n"
+	echo -e "\n### .bash_profile file ###"
 	diff -rq ~/.bash_profile ~/My-dotfiles/.bash_profile
 
-	echo -e "### .xinitrc files ###\n"
+	echo -e "\n### .xinitrc files ###"
 	diff -rq ~/.xinitrc ~/My-dotfiles/.xinitrc
 	diff -rq ~/.xinitrc_a ~/My-dotfiles/.xinitrc_a
 	diff -rq ~/.xinitrc_d ~/My-dotfiles/.xinitrc_d
 	diff -rq ~/.xinitrc_q ~/My-dotfiles/.xinitrc_q
 
-	echo -e "### .zprofile file ###\n"
+	echo -e "\n### .zprofile file ###"
 	diff -rq ~/.zprofile ~/My-dotfiles/.zprofile
 
-	echo -e "### .zshrc file ###\n"
+	echo -e "\n### .zshrc file ###"
 	diff -rq ~/.zshrc ~/My-dotfiles/.zshrc
 
 	echo -e "###################################################################\n"
 
 	echo -e "############################"
 	echo -e "### /USR DIRECTORY FILES ###"
-	echo -e "############################\n"
+	echo -e "############################"
 
-	echo -e "### sddm conf dir (/usr/share) ###\n"
+	echo -e "\n### sddm conf dir (/usr/share) ###"
 	diff -rq /usr/share/sddm ~/My-dotfiles/usr/share/sddm
 
-	echo -e "### sddm conf dir (/usr/lib/) ###\n"
+	echo -e "\n### sddm conf dir (/usr/lib/) ###"
 	diff -rq /usr/lib/sddm/sddm.conf.d ~/My-dotfiles/usr/lib/sddm/sddm.conf.d
 
 }
