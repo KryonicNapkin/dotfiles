337249c30b8f991d12bb22118a52cc51a96ebe53
coevoe <grastlinka@gmail.com>
2023-07-21 19:20:18 +0200

    Removed xprop-/_info.sh scripts

--- local/bin/xprop_info.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WINDOW=$(xprop -root | grep _NET_ACTIVE_WINDOW | head -1 | sed 's/.* //')
-
-xprop -id $WINDOW | grep -v _NET_WM_ICON > /tmp/xprop-info.txt
-
-notify-send -u normal -t 5000 "$(basename /tmp/xprop-info.txt)" "$(cat /tmp/xprop-info.txt)" 
