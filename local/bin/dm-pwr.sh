2a34ad9cc58a8669480411fd068f686e6543d08c
coevoe <grastlinka@gmail.com>
2023-07-21 19:26:32 +0200

    Added powermenu for dmenu

---
+++ local/bin/dm-pwr.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Debugging
+# set -xe
+
+# Imports
+. ~/.local/share/univ/vars
+
+# Variables
+# Options
+options=$(printf "Shutdown\\nReboot\\nLock\\nSuspend\\nLogout\\n" | sort | $dmdpy "Action:")
+
+# Start of Code
+case "$options" in
+"Shutdown")
+    systemctl poweroff
+    ;;
+"Reboot")
+    systemctl reboot
+    ;;
+"Lock")
+    betterlockscreen -l
+    ;;
+"Suspend")
+    amixer -q set Master toggle && systemctl suspend
+    ;;
+"Logout")
+    loginctl terminate-session ${XDG_SESSION_ID-}
+    ;;
+*)
+    echo "Invalid option"
+    ;;
+esac
+
+# End of Code
