5a2ef1368a65c719595bb6b53e6aad675a6f81fc
coevoe <85091338+KryonicNapkin@users.noreply.github.com>
2025-02-11 15:55:54 +0100

    Reshuffle lines

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,17 +8,20 @@ require "user.treesitter"
 require "user.autopairs"
 require "user.lualine"
 require "user.comment"
-vim.g.arduino_serial_cmd = 'picocom {port} -b {baud} -l'
-vim.opt.termguicolors = true
-require("bufferline").setup{}
-vim.cmd('colorscheme onedark')
-vim.cmd('set colorcolumn=80')
+
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
 		vim.cmd("ColorizerToggle")
 	end,
 })
--- c3 programming language treesitter
+
+vim.g.arduino_serial_cmd = 'picocom {port} -b {baud} -l'
+vim.opt.termguicolors = true
+require("bufferline").setup{}
+require("todo-comments").setup{}
+vim.cmd('colorscheme onedark')
+vim.cmd('set colorcolumn=80')
+-- treesitter for c3 programming language 
 vim.filetype.add({
   extension = {
     c3 = "c3",
