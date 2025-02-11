33a676b9b0b3751e5087f927cfdfc7c1f2fafe03
coevoe <85091338+KryonicNapkin@users.noreply.github.com>
2025-02-11 15:56:48 +0100

    Added dashboard-nvim and todo-comments.nvim

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -115,6 +115,39 @@ return packer.startup(function(use)
         "nvim-treesitter/nvim-treesitter",
         run = ":TSUpdate"
     }
+    -- TODO comments
+    use {
+        "folke/todo-comments.nvim",
+        requires = { "nvim-lua/plenary.nvim" },
+    }
+    -- Dashboard
+    use {
+        "nvimdev/dashboard-nvim",
+        event = 'VimEnter',
+        config = function()
+            require('dashboard').setup {
+              -- config 
+                theme = 'hyper', --  theme is doom and hyper default is hyper
+                disable_move = false,   --  default is false disable move keymap for hyper
+                shortcut_type = 'number',  --  shorcut type 'letter' or 'number'
+                shuffle_letter = true,--  default is true, shortcut 'letter' will be randomize, set to false to have ordered letter.
+                change_to_vcs_root = false, -- default is false,for open file in hyper mru. it will change to the root of vcs
+                config = {},    --  config used for theme
+                hide = {
+                  statusline,    -- hide statusline default is true
+                  tabline,       -- hide the tabline
+                  winbar,        -- hide winbar
+                },
+                preview = {
+                  command,       -- preview command
+                  file_path,     -- preview file path
+                  file_height,   -- preview file height
+                  file_width,    -- preview file width
+                },
+            }
+        end,
+        requires = {'nvim-tree/nvim-web-devicons'}
+    }
     -- Commenuse 
     use {
         "akinsho/toggleterm.nvim",
